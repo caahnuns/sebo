1d69544a5391d084c47e1e2c9c68bfcac851b5aa
Camila Melo <camila.crnm@gmail.com>
2023-03-29 20:50:04 -0300

    Implementando service e controller de Livro

---
+++ src/main/java/br/com/ada/sebo/controller/LivroController.java
@@ -0,0 +1,99 @@
+package br.com.ada.sebo.controller;
+
+import br.com.ada.sebo.model.dto.LivroDTO;
+import br.com.ada.sebo.model.dto.MensagemDTO;
+import br.com.ada.sebo.service.LivroService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/livros")
+public class LivroController {
+
+    @Autowired
+    LivroService service;
+
+    @GetMapping()
+    public ResponseEntity<Object> listar() {
+        try {
+            return ResponseEntity.ok(service.listar());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> listar(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(service.listar(id));
+        } catch (EntityNotFoundException e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .body(new MensagemDTO(e.getMessage()));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(e.getMessage()));
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> criar(@RequestBody @Valid LivroDTO livroDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(service.criar(livroDTO));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(e.getMessage()));
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> editar(@RequestBody @Valid LivroDTO livroDTO,
+                                         @PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(service.editar(livroDTO, id));
+        } catch (EntityNotFoundException e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(e.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletar(@PathVariable("id") Long id) {
+        try {
+            service.deletar(id);
+            return ResponseEntity.ok(new MensagemDTO("Livro #" + id + " removido com sucesso!"));
+        } catch (EntityNotFoundException e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new MensagemDTO(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new MensagemDTO(e.getMessage()));
+        }
+    }
+}
---
+++ src/main/java/br/com/ada/sebo/service/LivroService.java
@@ -0,0 +1,68 @@
+package br.com.ada.sebo.service;
+
+import br.com.ada.sebo.model.dto.LivroDTO;
+import br.com.ada.sebo.model.entity.LivroEntity;
+import br.com.ada.sebo.model.mapper.LivroMapper;
+import br.com.ada.sebo.repository.LivroRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    LivroRepository repository;
+
+    @Autowired
+    LivroMapper mapper;
+
+    public List<LivroDTO> listar() {
+        List<LivroEntity> entities = repository.findAll();
+        return mapper.updateListToDTO(entities);
+    }
+
+    public LivroDTO listar(Long id) {
+        Optional<LivroEntity> livro = repository.findById(id);
+
+        if(livro.isPresent()) {
+            LivroEntity entity = livro.get();
+            return mapper.update(entity);
+        }
+        throw new EntityNotFoundException("Ops... Livro não encontrado!");
+    }
+
+    public LivroDTO criar(LivroDTO livroDTO) {
+        LivroEntity entity = mapper.update(livroDTO);
+        entity = repository.save(entity);
+
+        return mapper.update(entity);
+    }
+
+    public LivroDTO editar(LivroDTO livroDTO, Long id) {
+        if(repository.existsById(id)) {
+            LivroEntity entity = mapper.update(livroDTO);
+            entity.setId(id);
+            entity = repository.save(entity);
+
+            return mapper.update(entity);
+        }
+        throw new EntityNotFoundException("Ops... Livro não encontrado!");
+    }
+
+    public void deletar(Long id) {
+        Optional<LivroEntity> livro = repository.findById(id);
+
+        if(livro.isPresent()) {
+            LivroEntity entity = livro.get();
+            repository.delete(entity);
+
+            return;
+        }
+        throw new EntityNotFoundException("Ops... Livro não encontrado!");
+    }
+}
